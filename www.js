e5d88c4230247e9db781f0b22d0dbbfc3cb8ba4d
Jarl Koha <jarkoha@users.noreply.github.com>
2023-10-05 17:29:26 +0300

    Viies tund tehtud

--- www.js
+++ www.js
@@ -3,6 +3,7 @@ const url = require("url");
 const path = require("path");
 const fs = require("fs");
 
+const querystring = require('querystring');
 const datetimeValue = require("./dateTimeET");
 const semesterInfo = require ("./semesterInfo");
 
@@ -11,14 +12,41 @@ const pageBanner = '\n\t<img src="banner.png" alt="Kursuse bänner">';
 const pageBody = '\n\t<h1 class="myname">Jarl Koha</h1>\n\t<p>See veebileht on valminud <a href="https://www.tlu.ee" target="_blank">TLÜ</a> Digitehnoloogiate instituudi informaatika eriala õppetöö raames.\n\t</p>';
 const pageTime = '\n\t<p>Lehe avamise hetkel oli kuupäev ' + datetimeValue.dateETFormatted() + ' ja kellaeg ' + datetimeValue.timeFormatted() + '\n</p>';   
 const pageFoot = '\n\t<hr>\n</body>\n</html>';
-    const tluPhoto = '\n\t<img src="tlu_38.jpg" alt="TLU foto">'
+const tluPhoto = '\n\t<img src="tlu_38.jpg" alt="TLU foto">'
 
 
 
 http.createServer(function(req, res){    //req = request, res = response/result
     let currentURL = url.parse(req.url, true);
     //console.log(currentURL);
-    if (currentURL.pathname === "/"){
+    if(req.method === 'POST'){
+        //res.end('Tuligi POST');
+		collectRequestData(req, result => {
+            console.log(result);
+            //kirjutame andmeid tekstifaili
+            fs.open('public/log.txt','a', (err, file)=>{
+                if (err) {
+                    throw err;
+                } else {
+                    fs.appendFile('public/log.txt', result.firstNameInput + ';', (err)=> {
+                        if (err) {
+                            throw err;
+                        } else {
+                            console.log('Faili kirjutati.')
+                        }
+                    });
+                    fs.close(file, (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }
+            });
+			res.end(result.firstNameInput);
+			//res.end('Tuligi POST!');
+		});
+    }
+    else if (currentURL.pathname === "/"){
         res.writeHead(200, {"Content-type": "text-html"});
         res.write(pageHead);
         res.write(pageBanner);
@@ -39,7 +67,7 @@ http.createServer(function(req, res){    //req = request, res = response/result
         res.write(pageBanner);
         res.write(pageBody);
         res.write('\n\t<hr>\n\t<h2>Lisa palun oma nimi</h2>');
-        res.write('\n\t<p>Edaspidi lisame siia asju</p>');
+        res.write('<form method="POST">\n\t<lable for="firstNameInput">Eesnimi: </lable>\n\t<input type="text" name="firstNameInput" id="firstnameInput" placeholder="Sinu eesnimi ...">\n\t<br>\n\t<lable for="lastNameInput">Perekonnanimi: </lable> <!--for väärtus on id väärtus >-->\n\t<input type="text" name="lastNameInput" id="lastNameInput" placeholder="Sinu perekonnanimi ...">\n\t<br>\n\t<input type="submit" name="nameSubmit" value="Salvesta">\n\t</form>');
         res.write(pageFoot);
         res.write(pageTime);
         return res.end();
@@ -64,13 +92,43 @@ http.createServer(function(req, res){    //req = request, res = response/result
     }
 
     else if (currentURL.pathname === "/tluphoto"){
-        res.writeHead(200, {"Content-type": "text-html"});
-        res.write(pageHead);
-        res.write(tluPhoto);
-        res.write(pageBody);
-        res.write(pageFoot);
-        res.write(pageTime);
-        return res.end();
+        //loeme kataloogist fotode nimekirja ja loosime ühe pildi
+        let htmlOut = '\n\t<p>Pilti ei saa näidata!</p>';
+        let listOutput = '';
+        fs.readdir('public/tluphotos', (err, fileList)=>{
+            if (err) {
+                throw err;
+                tluPhotoPage(res, htmlOut, listOutput);
+            } else {
+                //console.log(fileList);
+                let photoNum = Math.floor(Math.random() * fileList.length);
+                htmlOut = '\n\t<img src="' + fileList[photoNum] + '" alt="TLÜ pilt">';
+                listOutput = '\n\t<ul>';
+				for (fileName of fileList){
+					listOutput += '\n\t\t<li>' + fileName + '</li>';
+				}
+				listOutput += '\n\t</ul>';
+                //console.log(htmlOut);
+                //console.log(listOutput);
+                tluPhotoPage(res, htmlOut, listOutput);
+            }
+        });
+    }
+
+    //else if (currentURL.pathname === "/tlu_38.jpg"){
+    else if (path.extname(currentURL.pathname) === ".jpg"){
+        console.log(path.extname(currentURL.pathname));
+        let tluPhotoPath = path.join(__dirname, "public", "tluphotos");
+        console.log(tluPhotoPath + currentURL.pathname);
+        fs.readFile(tluPhotoPath + currentURL.pathname, (err, data)=>{
+            if (err) {
+                throw err;
+            }
+            else {
+                res.writeHead(200, {"Content-type": "image/jpg"});
+                res.end(data);
+            }
+        });
     }
 
     else if (currentURL.pathname === "/banner.png"){
@@ -88,26 +146,41 @@ http.createServer(function(req, res){    //req = request, res = response/result
         });
     }
 
-    else if (currentURL.pathname === "/tlu_38.jpg"){
-        console.log("Tahame pilti.");
-        let tluPhotoPath = path.join(__dirname, "public", "tluphotos/");
-        console.log(tluPhotoPath + currentURL.pathname);
-        fs.readFile(tluPhotoPath + currentURL.pathname, (err, data)=>{
-            if (err) {
-                throw err;
-            }
-            else {
-                res.writeHead(200, {"Content-type": "image/jpg"});
-                res.end(data);
-            }
-        });
-    }
-
     else {
         res.end("Error 404");
     }
     //valmis, saada ära
 }).listen(5134);
+
+function tluPhotoPage(res, htmlOut, listOutput) {
+    res.writeHead(200, {"Content-type": "text-html"});
+    res.write(pageHead);
+    res.write(pageBanner);
+    res.write(pageBody);
+    res.write(htmlOut);
+    if (listOutput != '') {
+        res.write(listOutput);
+    }
+    res.write(pageFoot);
+    res.write(pageTime);
+    return res.end();
+}
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let receivedData = '';
+        request.on('data', chunk => {
+            receivedData += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(querystring.decode(receivedData));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
 
 //rinde 5100
 //jarkoha 5134
